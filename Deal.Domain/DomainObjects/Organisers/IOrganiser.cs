543a84e2eb2043cd45850b6d1e0f8b8eb230b3a7
Steve Wright <stevewrightuk@gmail.com>
2020-02-25 18:23:12 +0000

    Create domain object Organiser (with tests)

---
+++ Deal.Domain/DomainObjects/Organisers/IOrganiser.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="IOrganiser.cs" company="Do It Wright">
+// Copyright (c) Do It Wright. All rights reserved.
+// </copyright>
+
+using System;
+
+namespace Deal.Domain.DomainObjects.Organisers
+{
+    /// <summary>
+    /// Organiser.
+    /// </summary>
+    public interface IOrganiser
+    {
+        /// <summary>
+        /// Gets the Organiser Id.
+        /// </summary>
+        Guid Id { get; }
+
+        /// <summary>
+        /// Gets the Organiser Code.
+        /// </summary>
+        string Code { get; }
+
+        /// <summary>
+        /// Gets the Organiser Name.
+        /// </summary>
+        string Name { get; }
+    }
+}
